267c956d88dd4032a3baa3ba55bc21c93277a004
Piaw Na <piaw.na@gmail.com>
2014-04-08 18:01:48 -0700

    modify scrip to break out giant array into smaller arrays

--- scripts/xmlify_food.py
+++ scripts/xmlify_food.py
@@ -2,7 +2,9 @@
 
 import fileinput
 
-def strip_newline(s):
+def process_food_names(s):
+    # clean up the & nonsense
+    s.replace('&', ' and ')
     if s[-1] == '\n':
         return s[0:-1]
     return s
@@ -10,18 +12,28 @@ def strip_newline(s):
 id_to_name_mapping = { }
 for lines in fileinput.input():
     food_id, food_name = lines.split('\001')
-    id_to_name_mapping[food_id] = strip_newline(food_name)
+    id_to_name_mapping[food_id] = process_food_names(food_name)
 
 print '<?xml version="1.0" encoding="utf-8"?>'
 print '<resources>'
-print '<string-array name="food_array">'
-key_list = []
-for key in id_to_name_mapping.keys():
-    print '<item>' + id_to_name_mapping[key] + '</item>'
-    key_list.append(key)
-print '</string-array>'
-print '<int-array name="food_id">'
-for key in key_list:
-    print '<item>' +key+'</item>'
-print '</int-array>'
+id_to_name_keys = id_to_name_mapping.keys()
+num_arrays = len(id_to_name_keys) / 500 + 1
+food_id = 0
+for array_num in range(0, num_arrays):
+    print '<string-array name="food_array_'+str(array_num)+'">'
+    key_list = []
+    if array_num < num_arrays - 1:
+        limit = 500
+    else:
+        limit = len(id_to_name_mapping) % 500
+    for i in range(0, limit):
+        index = array_num * 500 + i
+        key = id_to_name_keys[i]
+        print '<item>' + id_to_name_mapping[key] + '</item>'
+        key_list.append(key)
+    print '</string-array>'
+    print '<integer-array name="food_id_' + str(array_num)+'">'
+    for key in key_list:
+        print '<item>' +key+'</item>'
+    print '</integer-array>'
 print '</resources>'
