3974a36b6bfe250b71b0435b63256e650ca52989
Xiaoqin Ma <xiaoqin@couchbase.com>
2014-04-10 15:49:14 -0700

    Setup database in Nutrition TrackerApp and tested the food_names.

--- NutritionTracker/NutritionTracker/app/src/main/java/net/tigerparents/nut/StartScreen.java
+++ NutritionTracker/NutritionTracker/app/src/main/java/net/tigerparents/nut/StartScreen.java
@@ -1,10 +1,10 @@
 package net.tigerparents.nut;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.view.View;
 
 
